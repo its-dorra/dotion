11895a67bf00ceacc5b42cb3fe422f924bcd7843
Islam Abderrezak <islam.abderrezak@ensia.edu.dz>
2026-06-16 17:16:18 +0100

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,33 @@
+import { createDb } from "@notion-clone/db";
+import * as schema from "@notion-clone/db/schema/auth";
+import { env } from "@notion-clone/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [],
+  });
+}
+
+export const auth = createAuth();
